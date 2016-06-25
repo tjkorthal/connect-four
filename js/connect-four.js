6febcf11cd5d59502e445c081b36c7fbc6596ce2
Tyler Korthal <tjkorthal@live.com>
2016-06-25 01:03:50 -0400

    Add groundwork for scoring
    Add column and row indices for elements, add
    coordinates to hashes for each color's dots

--- js/connect-four.js
+++ js/connect-four.js
@@ -1,14 +1,16 @@
-var color, rows, columnsGlobal;
+var color, rows, columnsGlobal, redDots, yellowDots;
 color = "red";
 rows = 6;
 columnsGlobal = 7;
+redDots = [];
+yellowDots = [];
 
 function addDot(){
   var emptyDotArray = Array.from(this.querySelectorAll("div.dot:not(.red):not(.yellow)"));
   var emptyDot = emptyDotArray.pop();
   if (!emptyDot) return;
   emptyDot.className = "dot " + color;
-  //check for win
+  updateHash(this,emptyDot,color);
   checkWin();
   //switch color
   color = (color == "red" ? "yellow" : "red");
@@ -22,18 +24,25 @@ function addListeners(){
   }
 }
 
-function checkWin(){
+function capitalize(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
+function checkWin(){
+  //check hashes to see if a player won
 }
 
 function createBoard(){
   var target = document.querySelector("section.board");
+  target.innerHTML = "";
   for (var i = 0; i < columnsGlobal; i++) {
     var columnNode = document.createElement("div");
     columnNode.className = "column";
-    for (var j = 0; j < rows; j++) {
+    columnNode.setAttribute("data-column-index", i);
+    for (var j = rows-1; j >= 0; j--) {
       var dotNode = document.createElement("div");
       dotNode.className = "dot";
+      dotNode.setAttribute("data-row-index", j);
       columnNode.appendChild(dotNode);
     }
     target.appendChild(columnNode);
@@ -46,7 +55,14 @@ function initialize(){
   addListeners();
 }
 
+function updateHash(column, row, color) {
+  var x = column.getAttribute("data-column-index");
+  var y = row.getAttribute("data-row-index");
+  var hash = color == "red" ? redDots : yellowDots;
+  hash.push({"x": parseInt(x), "y": parseInt(y)});
+}
+
 function updateMessage(){
   var messageElement = document.querySelector("#flash");
-  messageElement.innerHTML = color + "'s turn";
+  messageElement.innerHTML = capitalize(color) + "'s turn";
 }
