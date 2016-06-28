020642f46d9fb7746204ad0fa73d0b026326830e
Tyler Korthal <tjkorthal@live.com>
2016-06-28 16:17:50 -0400

    Add logic for winning the game
    Refactor vertical win logic

--- js/connect-four.js
+++ js/connect-four.js
@@ -4,7 +4,8 @@ var color = "red",
     rows = 6,
     columnsGlobal = 7,
     redArray = [],
-    yellowArray = [];
+    yellowArray = [],
+    winner = false;
 
 function addDot(){
   var emptyDotArray = Array.from(this.querySelectorAll("div.dot:not(.red):not(.yellow)"));
@@ -13,9 +14,11 @@ function addDot(){
   emptyDot.className = "dot " + color;
   updateArray(this,emptyDot,color);
   checkWin();
-  //switch color
-  color = (color == "red" ? "yellow" : "red");
-  updateMessage();
+  if (winner == false){
+    //switch color
+    color = (color == "red" ? "yellow" : "red");
+    updateMessage();
+  }
 }
 
 function addListeners(){
@@ -29,109 +32,41 @@ function capitalize(string){
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-//http://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value-in-javascript
-function compare(a,b) {
-  if (a.x < b.x)
-    return -1;
-  if (a.x > b.x)
-    return 1;
-  return 0;
-}
-
-function compareArrays(a,b) {
-  if (a[0] < b[0])
-    return -1;
-  if (a[0] > b[0])
-    return 1;
-  return 0;
-}
-
 function checkWin(){
   var array = getColorArray(),
-      win = false,
-      consecutive = 1,
-      xCount = {},
-      yCount = {};
+      strategies;
   if (array.length < 4) {return;}
-  array.sort(compareArrays);
-  //count number dots per column
-  for (var i = 0; i < array.length; i++) {
-    //     currentY = array[i].y;
-    var currentX = array[i][0],
-        currentY = array[i][1];
-    if (xCount[currentX] === undefined){
-      xCount[currentX] = 1;
-    } else {
-      xCount[currentX]++;
-    }
-    if (yCount[currentY] === undefined){
-      yCount[currentY] = 1;
-    } else {
-      yCount[currentY]++;
-    }
-  }
-  console.log("X count: ");
-  console.log(xCount);
-  console.log("Y count: ");
-  console.log(yCount);
-
-  for (var property in xCount) {
-    if (xCount.hasOwnProperty(property)) {
-      if (xCount[property] > 3) {
-        checkVertical(array);
-      }
-    }
-  }
-
-  for (var property in yCount) {
-    if (yCount.hasOwnProperty(property)) {
-      if (yCount[property] > 3) {
-        checkHorizontal(array);
-      }
+  strategies = [verticalStrategy, horizontalStrategy, diagonalStrategy1, diagonalStrategy2];
+  for (var i = 0; i < strategies.length; i++) {
+    var strategy = strategies[i];
+    for (var j = 0; j < array.length; j++) {
+      var consecutive = 1,
+          xIndex = array[j][0],
+          yIndex = array[j][1];
+      consecutive = checkSequence(strategy, array, [xIndex,yIndex], consecutive);
+      if (consecutive == 4) {handleWin(color, strategy);}
     }
   }
 }
 
-function checkDiagonal(array) {
-
-}
-
-function checkHorizontal(array){
-  var keepSearching = true,
-      consecutive = 1,
-      xIndex,
-      yIndex;
-  for (var i = 0; i < array.length; i++) {
-    if (xIndex === undefined){ xIndex = array[i][0]; }
-    if (yIndex === undefined || yIndex != array[i][1]){
-      yIndex = array[i][1];
-      consecutive = 1;
-    }
-    keepSearching = (array[i][0] == xIndex+1) && (array[i][1] == yIndex);
-    if (!keepSearching) {continue;}
-    consecutive++;
-    yIndex++;
-    if (consecutive == 4) {alert(capitalize(color)+" wins!");}
+function checkSequence(method, array, coordinate, index){
+  if (containsCoordinate(array,coordinate)){
+    var newCoordinate = method(coordinate);
+    return index + checkSequence(method, array, newCoordinate, index)
+  } else {
+    return 0;
   }
 }
 
-function checkVertical(array){
-  var keepSearching = true,
-      consecutive = 1,
-      xIndex,
-      yIndex;
+function containsCoordinate(array, coordinate){
+  var x = coordinate[0],
+      y = coordinate[1];
   for (var i = 0; i < array.length; i++) {
-    if (xIndex === undefined || xIndex != array[i][0]){
-      xIndex = array[i][0];
-      consecutive = 1;
+    if ( (array[i][0] == x) && (array[i][1] == y) ){
+      return true;
     }
-    if (yIndex === undefined){ yIndex = array[i][1]; }
-    keepSearching = (array[i][0] == xIndex) && (array[i][1] == yIndex+1);
-    if (!keepSearching) {continue;}
-    consecutive++;
-    yIndex++;
-    if (consecutive == 4) {alert(capitalize(color)+" wins!");}
   }
+  return false;
 }
 
 function createBoard(){
@@ -151,16 +86,47 @@ function createBoard(){
   }
 }
 
+function diagonalStrategy1(coordinate){
+  return [coordinate[0]+1,coordinate[1]+1];
+}
+
+function diagonalStrategy2(coordinate){
+  return [coordinate[0]-1,coordinate[1]+1];
+}
+
+function horizontalStrategy(coordinate){
+  return [coordinate[0]+1,coordinate[1]];
+};
+
+function verticalStrategy(coordinate){
+  return [coordinate[0],coordinate[1]+1];
+};
+
 function getColorArray() {
   return color == "red" ? redArray : yellowArray;
 }
 
+function handleWin(color, strategy){
+  var messageElement = document.querySelector("#flash");
+  messageElement.innerHTML = capitalize(color) + " wins!";
+  removeListeners();
+  winner = true;
+}
+
 function initialize(){
+  winner = false;
   redArray = [];
   yellowArray = [];
   createBoard();
   updateMessage();
   addListeners();
+}
+
+function removeListeners(){
+  var columns = document.querySelectorAll(".column");
+  for (var i = columns.length - 1; i >= 0; i--) {
+    columns[i].removeEventListener("click", addDot);
+  }
 }
 
 function updateArray(column, row, color) {
