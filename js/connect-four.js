31b115fb8860533847139505ad515f9481edc0ad
Tyler Korthal <tjkorthal@live.com>
2017-07-04 00:04:20 -0400

    Clean up style, add NPM stuff and .gitignore

--- js/connect-four.js
+++ js/connect-four.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   let currentColor = "red",
       colorOne = "red",
       colorTwo = "yellow",
@@ -13,35 +13,35 @@
   initialize();
   document.getElementById("reset").addEventListener("click", initialize);
 
-  function addDot(){
+  function addDot() {
     const emptyDotArray = Array.from(this.querySelectorAll(
         `div.dot:not(.${colorOne}):not(.${colorTwo})`)),
-        emptyDot = emptyDotArray.pop(),
-        array = getColorArray();
+          emptyDot = emptyDotArray.pop(),
+          array = getColorArray();
     if (!emptyDot){ return; }
     emptyDot.className = `dot ${currentColor}`;
     updateArray(this, emptyDot, array);
     checkWin();
-    if (winner == false){
+    if (winner === false) {
       currentColor = switchColor(currentColor);
       updateElementbyId("flash", `${capitalize(currentColor)}'s turn`);
     }
   }
 
-  function addListeners(){
+  function addListeners() {
     const columns = document.querySelectorAll(".column");
     for (let i = columns.length - 1; i >= 0; i--) {
       columns[i].addEventListener("click", addDot);
     }
   }
 
-  function capitalize(string){
+  function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  function checkWin(){
+  function checkWin() {
     const array = getColorArray(),
-        strategies = [verticalStrategy, horizontalStrategy, diagonalStrategy1,
+          strategies = [verticalStrategy, horizontalStrategy, diagonalStrategy1,
                       diagonalStrategy2];
     if (array.length < 4) { return; }
     for (let i = 0; i < strategies.length; i++) {
@@ -55,33 +55,32 @@
     }
   }
 
-  function checkSequence(method, array, coordinate, index){
-    if (containsCoordinate(array, coordinate)){
-      let newCoordinate = method(coordinate);
+  function checkSequence(method, array, coordinate, index) {
+    if (containsCoordinate(array, coordinate)) {
+      const newCoordinate = method(coordinate);
       return index + checkSequence(method, array, newCoordinate, index);
-    } else {
-      return 0;
     }
+    return 0;
   }
 
-  function containsCoordinate(array, coordinate){
+  function containsCoordinate(array, coordinate) {
     for (let i = 0; i < array.length; i++) {
-      if ( (array[i]["x"] == coordinate["x"]) &&
-          (array[i]["y"] == coordinate["y"]) ){
+      if ((array[i]["x"] == coordinate["x"]) &&
+          (array[i]["y"] == coordinate["y"])) {
         return true;
       }
     }
     return false;
   }
 
-  function createBoard(){
+  function createBoard() {
     const target = document.querySelector("section.board");
     target.innerHTML = "";
     for (let i = 0; i < totalColumns; i++) {
       const columnNode = document.createElement("div");
       columnNode.className = "column";
       columnNode.setAttribute("data-column-index", i);
-      for (let j = totalRows-1; j >= 0; j--) {
+      for (let j = totalRows - 1; j >= 0; j--) {
         const dotNode = document.createElement("div");
         dotNode.className = "dot";
         dotNode.setAttribute("data-row-index", j);
@@ -91,31 +90,31 @@
     }
   }
 
-  function diagonalStrategy1(coordinate){
-    return {"x": coordinate["x"]+1,
-            "y": coordinate["y"]+1 };
+  function diagonalStrategy1(coordinate) {
+    return { "x": coordinate["x"] + 1,
+             "y": coordinate["y"] + 1 };
   }
 
-  function diagonalStrategy2(coordinate){
-    return {"x": coordinate["x"]-1,
-            "y": coordinate["y"]+1 };
+  function diagonalStrategy2(coordinate) {
+    return { "x": coordinate["x"] - 1,
+             "y": coordinate["y"] + 1 };
   }
 
-  function horizontalStrategy(coordinate){
-    return {"x": coordinate["x"]+1,
-            "y": coordinate["y"] };
-  };
-
-  function verticalStrategy(coordinate){
-    return {"x": coordinate["x"],
-            "y": coordinate["y"]+1 };
-  };
-
-  function getColorArray(){
-    return currentColor == colorOne ? colorOneArray : colorTwoArray;
+  function horizontalStrategy(coordinate) {
+    return { "x": coordinate["x"] + 1,
+             "y": coordinate["y"] };
   }
 
-  function handleWin(color){
+  function verticalStrategy(coordinate) {
+    return { "x": coordinate["x"],
+             "y": coordinate["y"] + 1 };
+  }
+
+  function getColorArray() {
+    return currentColor === colorOne ? colorOneArray : colorTwoArray;
+  }
+
+  function handleWin(color) {
     winner = true;
     updateElementbyId("flash", `${capitalize(color)} wins!`);
     color == colorOne ? colorOneWins++ : colorTwoWins++;
@@ -124,7 +123,7 @@
     removeListeners();
   }
 
-  function initialize(){
+  function initialize() {
     winner = false;
     colorOneArray = [];
     colorTwoArray = [];
@@ -134,25 +133,25 @@
     addListeners();
   }
 
-  function removeListeners(){
+  function removeListeners() {
     const columns = document.querySelectorAll(".column");
     for (let i = columns.length - 1; i >= 0; i--) {
       columns[i].removeEventListener("click", addDot);
     }
   }
 
-  function switchColor(color){
-    return (color == colorOne ? colorTwo : colorOne);
+  function switchColor(color) {
+    return (color === colorOne ? colorTwo : colorOne);
   }
 
   function updateArray(column, row, array) {
     const x = column.getAttribute("data-column-index"),
-        y = row.getAttribute("data-row-index");
-    array.push({ "x": parseInt(x), "y": parseInt(y)});
+          y = row.getAttribute("data-row-index");
+    array.push({ "x": parseInt(x), "y": parseInt(y) });
   }
 
-  function updateElementbyId(id, message){
-    let messageElement = document.querySelector("#"+id);
+  function updateElementbyId(id, message) {
+    const messageElement = document.querySelector("#" + id);
     messageElement.innerHTML = message;
   }
 }());
